4c1a17e5fa567fc840c920cb89ce2bc0805d6405
VysokikhAnna <anutka320@gmail.com>
2023-02-01 16:52:09 +0300

    first commit

---
+++ src/components/Order.js
@@ -0,0 +1,18 @@
+import { FaTrash } from "react-icons/fa"
+
+import React, { Component } from 'react'
+
+export class Order extends Component {
+  render() {
+    return (
+      <div className='item'>
+        <img src={'./img/' + this.props.item.img} alt='Товар'/>
+        <h2>{this.props.item.title}</h2>
+        <b>{this.props.item.price}$</b>
+        <FaTrash className="delete-icon" onClick={() => this.props.onDelete(this.props.item.id)}/>
+      </div>
+    )
+  }
+}
+
+export default Order
